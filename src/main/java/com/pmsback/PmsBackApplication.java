87697154c032f913fedbd6d6c44e7adb9676d67c
Sn0w_15 <81741053+Snowman1115@users.noreply.github.com>
2025-02-16 15:37:25 +0800

    Frontend & Backend Environment Setup - File Structure

---
+++ src/main/java/com/pmsback/PmsBackApplication.java
@@ -0,0 +1,13 @@
+package com.pmsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsBackApplication.class, args);
+    }
+
+}
